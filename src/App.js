9be7384dd214d0b9b4d6ab5e1d6d69993a5feafd
montazeri_a <montazeri_a@office.zharfpouyan.net>
2021-12-25 16:08:58 +0330

    =new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div data-test="component-app">
-      <h1>hh</h1>
+      <h1 data-test="counter-display">
+        this is hh
+        <span data-test="count">{count}</span>
+      </h1>
+      <button 
+        data-test="increment-button"
+        onClick={() => {setCount(count + 1)}}></button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,24 +4,42 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+}
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-  
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
 });
 
 test('renders counter starts at 0', () => {
-  
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
 });
 
 test('renders clicking button increment counter display ', () => {
-  
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+
+  button.simulate('click');
+
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(button.length).toBe(1);
 });
